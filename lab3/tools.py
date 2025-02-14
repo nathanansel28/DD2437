0b969f00504aae1e553af2a34e0c5348109daad2
Nathan Ansel <nathanansel.edu@gmail.com>
2025-02-14 16:25:09 +0100

    feat: Load data and add image visualizer.

--- lab3/tools.py
+++ lab3/tools.py
@@ -1,11 +1,19 @@
+from typing import Dict 
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 class HopfieldNetwork:
-    def __init__(self, n_nodes: int = 100, max_epochs: int = 20):
+    def __init__(
+        self, 
+        n_nodes: int = 100, 
+        max_epochs: int = 20
+    ):
         self.n_nodes = n_nodes
         self.epochs = max_epochs
         self.weights = np.zeros((n_nodes, n_nodes), dtype=np.float64)
+        self.p = self._init_p()
+
 
     def fit(self, patterns: np.ndarray) -> None:
         assert (
@@ -16,6 +24,7 @@ class HopfieldNetwork:
             self.weights += np.outer(pattern, pattern)
 
         self.weights /= self.n_nodes
+
 
     def recall(self, inputs: np.ndarray) -> np.ndarray:
         previous = np.zeros(inputs.shape)
@@ -31,3 +40,29 @@ class HopfieldNetwork:
             previous = current
 
         return previous
+
+
+    def visualize(
+        self, pattern: np.ndarray = None, pattern_index: int = None
+    ) -> None: 
+        """Visualizes a 1024-bit pattern as a 32x32 image."""
+        if pattern is None and pattern_index is None: 
+            raise TypeError(f"Either pattern or pattern_inde is required.")
+        if pattern is None: 
+            pattern = self.p[pattern_index]
+        image = pattern.reshape(32, 32)
+
+        plt.imshow(image, cmap='gray', vmin=-1, vmax=1)
+        plt.title("32x32 Image Visualization")
+        plt.colorbar(label="Value (-1 or 1)")
+        plt.show()
+
+
+    def _init_p(self) -> Dict[int, np.ndarray]: 
+        data = np.loadtxt('pict.dat', delimiter=',')
+        assert len(data) == 1024 * 11, "The data length is not 1024 * 11."
+        patterns = np.split(data, 11)
+        return {i + 1: patterns[i] for i in range(11)}
+    
+
+    
